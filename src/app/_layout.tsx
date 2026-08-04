9b3112c06d682f503da162bbdc9a8d51934fad9e
Ricardo Peixoto <rkrdodev@gmail.com>
2026-08-03 21:05:27 -0300

    fontes e loading

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,28 @@
-import {Stack} from 'expo-router'
-export default function RootLayout() {
-    return <Stack screenOptions={{headerShown:false}} />
+import { Stack } from "expo-router";
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_700Bold,
+  useFonts,
+} from "@expo-google-fonts/inter";
+import { colors } from "@/theme/colors";
+import { Loading } from "@/components/loading";
+export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_700Bold,
+  });
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: colors.white },
+      }}
+    />
+  );
+  
 }
---
+++ src/theme/fonsts.ts
@@ -0,0 +1,6 @@
+export const fonts = {
+    regular: 'Inter_400Regular',
+    medium: 'Inter_500Medium',
+    bold: 'Inter_700Bold',
+    
+}
